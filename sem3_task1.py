7e53a718ec3999c5bd82c3e2c6dc083f22d42ead
CyberShur <102461318+CyberShur@users.noreply.github.com>
2022-09-03 20:16:11 +0300

    решена первая задача

--- sem3_task1.py
+++ sem3_task1.py
@@ -4,12 +4,14 @@
 - [2, 3, 5, 9, 3] -> на нечётных позициях элементы 3 и 9, ответ: 12
 '''
 
-x = [2, 3, 5, 9, 3, 5, 3]
-length = len(x)
-res = 0
+x = [2, 1, 5, 9, 3, 5, 7, 5]
 
-for i in range(length):
-    if i%2 != 0:
-        res = res + x[i]
+def result(a):
+    length = len(a)
+    res = 0
+    for i in range(length):
+        if i%2 != 0:
+            res = res + a[i]
+    return res
 
-print(res)
+print(result(x))
